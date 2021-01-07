a624ee9ed6d3cde109e040a844ff96c5bb877ac0
jwo29 <55731054+jwo29@users.noreply.github.com>
2021-01-07 19:11:13 +0900

    Baekjoon 1920(Binary Search) Python Solution

---
+++ backjoon1920_SearchNumber.py
@@ -0,0 +1,22 @@
+# 수 찾기
+def binary_search(target):
+    s = 0
+    e = len(numbers) - 1
+    while s <= e:
+        middle = (s + e) // 2
+
+        if target == numbers[middle]:
+            return 1
+        elif target < numbers[middle]:
+            e = middle - 1
+        else:
+            s = middle + 1
+    return 0
+
+input()
+numbers = sorted(list(map(int, input().split())))
+input()
+targets = list(map(int, input().split()))
+
+for t in targets:
+    print(binary_search(t))
